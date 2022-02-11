b8eb18504aca575de39894569331d6315a0bb5c0
loicmaes <97891718+loicmaes@users.noreply.github.com>
2022-02-11 19:45:50 +0100

    :rocket: Project initialization

---
+++ src/views/Game.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="game">
+    <section class="game__content">
+      <h1 class="game__content__title">MineSwiper</h1>
+      <div class="game__content__grid">
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+        <span class="game__content__grid__cell"></span>
+      </div>
+    </section>
+
+    <aside class="game__scoreboard">
+
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game'
+}
+</script>
+
+<style scoped lang="scss">
+.game {
+  display: flex;
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+
+    &__title {
+
+    }
+
+    &__grid {
+      display: grid;
+      width: 20rem;
+      height: 20rem;
+      grid-template-rows: repeat(10, 1fr);
+      grid-template-columns: repeat(10, 1fr);
+    }
+  }
+}
+</style>
